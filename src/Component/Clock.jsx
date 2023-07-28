14385c61fcccf925e858ea7f9810e13359898427
Sajol karmakar <sajolkar56@gmail.lcom>
2023-07-28 15:58:53 +0600

    state and life cycle in class component

--- src/Component/Clock.jsx
+++ src/Component/Clock.jsx
@@ -2,11 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 class Clock extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { date: new Date() };
+    // }
+    state = { date: new Date() };
+
+    componentDidMount() {
+        this.clockRef = setInterval(() => { this.updateTime() }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.clockRef);
+    }
+
+    updateTime() {
+        this.setState({
+            date: new Date(),
+        })
+    }
     render() {
         return (
             <div>
                 <h1 className='heading'>
-                    <span className='text'>{this.props.children}: {new Date().toLocaleTimeString(this.props.localet)} </span>
+                    <span className='text'>{this.props.children}: {this.state.date.toLocaleTimeString(this.props.localet)} </span>
                 </h1>
             </div>
         );
