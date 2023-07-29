c1f55f5a2926c2f341c1318e2855295697b5df23
Sajol karmakar <sajolkar56@gmail.lcom>
2023-07-29 11:27:49 +0600

    event handaling

--- src/Component/Clock.jsx
+++ src/Component/Clock.jsx
@@ -6,7 +6,7 @@ class Clock extends React.Component {
     //     super(props);
     //     this.state = { date: new Date() };
     // }
-    state = { date: new Date() };
+    state = { date: new Date(), language: 'en-US' };
 
     componentDidMount() {
         this.clockRef = setInterval(() => { this.updateTime() }, 1000);
@@ -21,12 +21,25 @@ class Clock extends React.Component {
             date: new Date(),
         })
     }
+    changeLanguage = (language) => {
+        if (language == 'bn-BD') {
+            this.setState({
+                language: 'en-BD'
+            });
+        } else {
+            this.setState({
+                language: 'bn-BD'
+            });
+        }
+    }
     render() {
+        const { date, language } = this.state;
         return (
             <div>
                 <h1 className='heading'>
-                    <span className='text'>{this.props.children}: {this.state.date.toLocaleTimeString(this.props.localet)} </span>
+                    <span className='text'>{this.props.children}: {date.toLocaleTimeString(language)} </span>
                 </h1>
+                <button onClick={() => this.changeLanguage(language)}>Change Language</button>
             </div>
         );
     }
